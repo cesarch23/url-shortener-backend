95372538e5de42da01505731e8e9b9a3cebb57d5
Cesar Choccata <c21choccata@gmail.com>
2026-02-01 22:56:00 -0500

    Add getters and setters for Link entity

--- src/main/java/shorty/com/urlshortener/entity/Link.java
+++ src/main/java/shorty/com/urlshortener/entity/Link.java
@@ -3,6 +3,7 @@ package shorty.com.urlshortener.entity;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -32,6 +33,74 @@ public class Link {
     )
     public User user;
 
+    public Link (){}
 
+    public Link(UUID id, String originalUrl, String code, LocalDateTime createdAt, LocalDateTime expiredAt, User user) {
+        this.id = id;
+        this.originalUrl = originalUrl;
+        this.code = code;
+        this.createdAt = createdAt;
+        this.expiredAt = expiredAt;
+        this.user = user;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Link link)) return false;
+        return Objects.equals(id, link.id) && Objects.equals(originalUrl, link.originalUrl) && Objects.equals(code, link.code) && Objects.equals(createdAt, link.createdAt) && Objects.equals(expiredAt, link.expiredAt) && Objects.equals(user, link.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, originalUrl, code, createdAt, expiredAt, user);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getExpiredAt() {
+        return expiredAt;
+    }
+
+    public void setExpiredAt(LocalDateTime expiredAt) {
+        this.expiredAt = expiredAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
