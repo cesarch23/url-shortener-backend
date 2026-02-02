ffa512bf5642aaabed3220d1da1461aebd231067
Cesar Choccata <c21choccata@gmail.com>
2026-02-01 22:56:13 -0500

    Add getters and setters for User entity

--- src/main/java/shorty/com/urlshortener/entity/User.java
+++ src/main/java/shorty/com/urlshortener/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -24,4 +25,76 @@ public class User {
     public LocalDateTime createdAt;
     @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
     public List<Link> links;
+
+    public User(){}
+    public User(UUID id, String email, String name, String lastname, LocalDateTime createdAt, List<Link> links) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.lastname = lastname;
+        this.createdAt = createdAt;
+        this.links = links;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getName(), user.getName()) && Objects.equals(getLastname(), user.getLastname()) && Objects.equals(getCreatedAt(), user.getCreatedAt()) && Objects.equals(getLinks(), user.getLinks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getEmail(), getName(), getLastname(), getCreatedAt(), getLinks());
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+
+
 }
